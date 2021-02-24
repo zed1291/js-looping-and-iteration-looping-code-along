87591de8c2ac71c619b49b7f1a691d3264658021
“zed” <zed@kolk.me>
2021-02-23 18:09:38 -0600

    commit tree 5ea477190d330e77c8e2e1b7eadaa6402374320f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let cards = []
+    for ( let i = 0; i < names.length; i++ ) {
+        cards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+}
+
+  
+//   function countDown( startingNumber ) {
+//     while ( startingNumber > 0 ) {
+//       console.log( startingNumber );
+//       startingNumber -= 1;
+//     }
+//     console.log( startingNumber );
+//   }
