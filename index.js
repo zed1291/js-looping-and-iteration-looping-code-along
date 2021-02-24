40dd67b7f400334192cbb1c0e24d49741cb8c6f3
“zed” <zed@kolk.me>
2021-02-23 18:10:19 -0600

    le fin

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let cards = []
+    for ( let i = 0; i < names.length; i++ ) {
+        cards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number)
+        number -= 1
+    }
+    console.log(number)
+}
